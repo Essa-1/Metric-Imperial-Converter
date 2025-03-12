9f083a4295c0ba3f6ab6cdddf8dec2834802fd17
eshasahu-21 <sahuesha882@gmail.com>
2025-03-12 23:14:29 +0530

    Updated functional_tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,7 @@ const assert = chai.assert;
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
+  
   test("Convert a valid input", function (done) {
     chai
       .request(server)
@@ -13,6 +14,7 @@ suite("Functional Tests", function () {
       .end(function (err, res) {
         assert.equal(res.status, 200);
         assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, "L");
         done();
       });
   });
@@ -20,9 +22,41 @@ suite("Functional Tests", function () {
   test("Convert an invalid unit", function (done) {
     chai
       .request(server)
-      .get("/api/convert?input=10g")
+      .get("/api/convert?input=32g")
       .end(function (err, res) {
         assert.equal(res.body.error, "invalid unit");
+        done();
+      });
+  });
+
+  test("Convert an invalid number", function (done) {
+    chai
+      .request(server)
+      .get("/api/convert?input=3/7.2/4kg")
+      .end(function (err, res) {
+        assert.equal(res.body.error, "invalid number");
+        done();
+      });
+  });
+
+  test("Convert an invalid number AND unit", function (done) {
+    chai
+      .request(server)
+      .get("/api/convert?input=3/7.2/4kilomegagram")
+      .end(function (err, res) {
+        assert.equal(res.body.error, "invalid number and unit");
+        done();
+      });
+  });
+
+  test("Convert with no number", function (done) {
+    chai
+      .request(server)
+      .get("/api/convert?input=kg")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, "kg");
         done();
       });
   });
